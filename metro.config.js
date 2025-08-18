9dd1593fff965769d085ffc50cd3f3fdaf8fabc2
Noxonet <ajsa1persent9@gmail.com>
2025-08-18 09:38:40 +0330

    #40

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,7 @@
-// metro.config.js
 const { getDefaultConfig } = require("@react-native/metro-config");
-const JavaScriptObfuscator = require('javascript-obfuscator');
-
-const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = {
-  ...defaultConfig,
   transformer: {
-    ...defaultConfig.transformer,
-    babelTransformerPath: require.resolve('./transformer-obfuscator.js'),
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
@@ -17,7 +10,6 @@ module.exports = {
     }),
   },
   resolver: {
-    ...defaultConfig.resolver,
     sourceExts: ['tsx', 'ts', 'jsx', 'js', 'json'],
   },
 };
