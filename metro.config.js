908aaadda6c8619f6bc288e4cb3137db8a1d0e78
Noxonet <ajsa1persent9@gmail.com>
2025-08-18 19:24:52 +0330

    #47

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,19 @@
 const { getDefaultConfig } = require("@react-native/metro-config");
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  return {
+    ...config,
+    resolver: {
+      ...config.resolver,
+      extraNodeModules: {
+        ...config.resolver.extraNodeModules,
+        // ✅ هرجا react-native-paper اشتباه material-design-icons رو صدا بزنه
+        // به MaterialCommunityIcons اصلی هدایت میشه
+        "@react-native-vector-icons/material-design-icons":
+          require.resolve("react-native-vector-icons/MaterialCommunityIcons"),
       },
-    }),
-  },
-  resolver: {
-    sourceExts: ['tsx', 'ts', 'jsx', 'js', 'json'],
-  },
-};
+    },
+  };
+})();
