457d60faecbd0597c0c1be1ab9ad397af5d66b41
Noxonet <ajsa1persent9@gmail.com>
2025-08-21 23:46:13 +0330

    #121

--- android/app/src/main/java/com/yaranahapp/MainApplication.kt
+++ android/app/src/main/java/com/yaranahapp/MainApplication.kt
@@ -1,38 +1,69 @@
 package com.yaranahapp
 
 import android.app.Application
+import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.config.ReactFeatureFlags
+import com.facebook.soloader.SoLoader
+import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+        override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
+            // Add additional packages here if needed
+            return packages
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+
+        override fun isHermesEnabled(): Boolean {
+            return BuildConfig.IS_HERMES_ENABLED
+        }
+
+        override fun isNewArchEnabled(): Boolean {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+        initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+
+    companion object {
+        private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
+            if (BuildConfig.DEBUG) {
+                try {
+                    val aClass = Class.forName("com.yaranahapp.ReactNativeFlipper")
+                    aClass
+                        .getMethod("initializeFlipper", Context::class.java, ReactInstanceManager::class.java)
+                        .invoke(null, context, reactInstanceManager)
+                } catch (e: ClassNotFoundException) {
+                    e.printStackTrace()
+                } catch (e: NoSuchMethodException) {
+                    e.printStackTrace()
+                } catch (e: IllegalAccessException) {
+                    e.printStackTrace()
+                } catch (e: InvocationTargetException) {
+                    e.printStackTrace()
+                }
             }
-
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+        }
+    }
 }
